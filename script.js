9f5505420575a9df4ad029365bb4ed98decd69b6
Rizuan <john.lee.coco@gmail.com>
2023-12-25 04:32:18 +0800

    Add modal with form for adding book to library
    - Clicking on 'Add a book' now triggers a modal for adding a book to the
    library. Functionality will be built in a future commit.
    - Added general styling tweaks to buttons.

--- script.js
+++ script.js
@@ -6,8 +6,45 @@ function Book(title, author, pages) {
     this.pages = pages;
 }
 
-
-
 function addBookToLibrary(book) {
     
 }
+
+function initialise() {
+    const addBookButtons = document.querySelectorAll("[data-modal-target]");
+    const overlay = document.querySelector("#overlay");
+
+    addBookButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const modal = document.querySelector(button.dataset.modalTarget);
+            openModal(modal);
+        });
+    });
+
+    overlay.addEventListener("click", () => {
+        const modals = document.querySelectorAll(".modal.active");
+        modals.forEach(modal => {
+            closeModal(modal);
+        });
+    });
+}
+
+function openModal(modal) {
+    if (modal == null) {
+        return;
+    } else {
+        modal.classList.add("active");
+        overlay.classList.add("active");
+    }
+}
+
+function closeModal(modal) {
+    if (modal == null) {
+        return;
+    } else {
+        modal.classList.remove("active");
+        overlay.classList.remove("active");
+    }
+}
+
+initialise();
